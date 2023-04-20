6e0408e79ec7f793d9517672410979ca28cc6a4e
irodrigon <irodrigon2313@gmail.com>
2023-04-20 13:41:39 +0200

    Second commit Agent class

--- SCP/src/clases/Agent.java
+++ SCP/src/clases/Agent.java
@@ -1,5 +1,8 @@
 package clases;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import controller.AgentController;
 import controller.Loginable;
 
@@ -11,6 +14,29 @@ public class Agent extends Worker implements AgentController{
 		
 	}*/
 	
+	private String id_facility;
+	private String history;
+	
+	private Connection con;
+	private PreparedStatement stmt;
+	private DBConnectionController conController = new DBConnectionController();
+	
+	public String getId_facility() {
+		return id_facility;
+	}
+
+	public void setId_facility(String id_facility) {
+		this.id_facility = id_facility;
+	}
+
+	public String getHistory() {
+		return history;
+	}
+
+	public void setHistory(String history) {
+		this.history = history;
+	}
+
 	@Override
 	public void showAsignedFacility() {
 		// TODO Auto-generated method stub
